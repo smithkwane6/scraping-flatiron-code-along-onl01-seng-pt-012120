2579c23e983fc8bd5a6c078a259642d49d0fb00b
Kwane Smith <smithkwane6@gmail.com>
2020-02-26 19:36:10 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -7,12 +7,11 @@ class Course
     @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 
-  def self.reset_all 
+  def self.reset_all
     @@all.clear
   end
 end
-
